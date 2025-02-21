cef9becd49b94817a18abdc788e02be1c11ad82e
Édito Silva <edito.desenvolvedor@gmail.com>
2025-02-21 08:51:25 -0300

    Ajustes finais

--- src/services/costumer/investments/ApiInvestiment.ts
+++ src/services/costumer/investments/ApiInvestiment.ts
@@ -3,6 +3,7 @@ import ApiService from '@/services/ApiService';
 import endpointConfig from '@/configs/costumer/endpointConfig';
 
 import { InvestmentResponse } from '@/@types/costumer/investiment/InvestMentTypes';
+
 export async function apiGetInvestiment() {
     return ApiService.fetchDataWithAxios<InvestmentResponse>({
         url: endpointConfig.listInvestments,
