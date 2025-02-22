9dc07621fa61afd293e27a8f6adbe50334ac6b5a
Édito Silva <edito.desenvolvedor@gmail.com>
2025-02-21 21:49:44 -0300

    Alteçãoes

--- src/configs/base.ts
+++ src/configs/base.ts
@@ -1,6 +1,7 @@
 
 export default {
-    baseUrl: 'https://api.neloreinvest.com/v1'
+    // baseUrl: 'https://api.neloreinvest.com/v1'
     // baseUrl: 'http://localhost:8787/v1'
+    baseUrl: import.meta.env.VITE_API_URL
 
 }
