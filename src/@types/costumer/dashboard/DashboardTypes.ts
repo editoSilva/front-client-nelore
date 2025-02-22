24b9dccf5e743ec2347c357effed6decbce8f232
Édito Silva <edito.desenvolvedor@gmail.com>
2025-02-22 10:21:24 -0300

    Ajuestes

--- src/@types/costumer/dashboard/DashboardTypes.ts
+++ src/@types/costumer/dashboard/DashboardTypes.ts
@@ -1,14 +1,15 @@
 export type DashboardTypes = {
-    total_network: number,
-    referrer: string,
-    name: string,
-    walletBalance: number,
-    totalInvested: number,
-    investmentReturns: number,
-    affiliateLink: string,
-    visited: number,
-    last_visited: number | null,
-    rate: number,
-    referralBonus: number,
+    total_network: number
+    referrer: string
+    name: string
+    walletBalance: number
+    totalInvested: number
+    investmentReturns: number
+    affiliateLink: string
+    visited: number
+    last_visited: number | null
+    rate: number
+    referralBonus: number
     dailyPrice: number
+    withdrawl:number
 }
