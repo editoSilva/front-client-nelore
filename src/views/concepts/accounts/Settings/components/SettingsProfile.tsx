cd2f237dcafea5a2c857e82a73754794fd4e138c
Édito Silva <edito.desenvolvedor@gmail.com>
2025-02-25 19:26:53 -0300

    AJustes

--- src/views/concepts/accounts/Settings/components/SettingsProfile.tsx
+++ src/views/concepts/accounts/Settings/components/SettingsProfile.tsx
@@ -472,7 +472,7 @@ const SettingsProfile = () => {
                         type="submit"
                         loading={isSubmittingPix}
                     >
-                       Enviar
+                       Atualizar
                     </Button>
                 </div>
                 </Form>
