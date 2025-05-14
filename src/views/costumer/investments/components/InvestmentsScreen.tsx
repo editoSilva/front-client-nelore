e927ceb406d2dc0434c3dfb329ff1af616a35d27
Édito Silva <edito.desenvolvedor@gmail.com>
2025-05-14 14:15:52 -0300

    alteaões

--- src/views/costumer/investments/components/InvestmentsScreen.tsx
+++ src/views/costumer/investments/components/InvestmentsScreen.tsx
@@ -184,11 +184,11 @@ const InvestmentsScreen = () => {
                               <Td>
                                 {card.status === 'paid' ? (
                                   <Tag className="bg-emerald-100 text-emerald-600 dark:bg-emerald-500/20 p-2 dark:text-emerald-100 border-0 rounded">
-                                      {card.status}
+                                      Pago
                                   </Tag>
                                    ) : (     
-                                    <Tag className="bg-red-100 text-red-600 dark:bg-red-500/20 p-2 dark:text-red-100 border-0 rounded">
-                                        {card.status}
+                                    <Tag className="bg-red-100 text-orange-600 dark:bg-red-500/20 p-2 dark:text-red-100 border-0 rounded">
+                                       Aguardando
                                     </Tag>
                                 )}
                               </Td>         
